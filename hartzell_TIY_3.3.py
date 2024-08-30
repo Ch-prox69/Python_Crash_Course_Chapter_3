bb51650064bea061470bf57c472268755088c129
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 18:33:29 -0400

    Add files via upload

---
+++ hartzell_TIY_3.3.py
@@ -0,0 +1,13 @@
+# Simple List on favorite modes of transportation
+modes_of_transportation = ['Car', 'Bus', 'Taxi', 'Train']
+
+# How I feel about these modes of transportation
+statements = [
+    f"My favorite mode of transportation is {modes_of_transportation[0].title()}.",
+    f"My second favorite mode of transportation is {modes_of_transportation[2].title()}.",
+    f"My third favorite mode of transportation is {modes_of_transportation[1].title()}.",
+    f"My fourth favorite mode of transportation is {modes_of_transportation[3].title()}.",
+]
+
+# Printing 
+print(statements)
