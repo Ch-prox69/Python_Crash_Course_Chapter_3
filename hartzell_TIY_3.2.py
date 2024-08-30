6f06e9245cf266258377450735ee880576ad9b51
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 18:08:37 -0400

    Add files via upload

---
+++ hartzell_TIY_3.2.py
@@ -0,0 +1,16 @@
+# List of names of friends 
+names = ['John', 'Quinn', 'Peyton', 'Daniel', 'Jacob', 'Aiden']
+
+# List of messeges to friends
+messages = [
+    f"I like your truck {names[0].title()}.",
+    f"You never back down from your views {names[1].title()}.",
+    f"You've always been a loyal friend {names[2].title()}.",
+    f"Whenever I'm around you, I laugh with you  {names[3].title()}.",
+    f"I've always stood up for you and I will continue to do so  {names[4].title()}.",
+    f"You can be a block-head sometimes, but I know if I'm ever in trouble, I can count on you {names[5].title()}.",
+]
+
+# Printing
+print(messages)
+
