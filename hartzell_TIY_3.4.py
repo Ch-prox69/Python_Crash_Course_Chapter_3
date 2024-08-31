5aecf874f119aeca2a80eeb549f93a8f74383f62
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-08-30 21:42:52 -0400

    Add files via upload

---
+++ hartzell_TIY_3.4.py
@@ -0,0 +1,10 @@
+# The guests 
+guests = ['Heisenburg', 'Homelander', 'Patrick Bateman']
+
+# The message to the guests 
+the_invitation = [
+    f"Dear {guests[0].title()} you are invited to the upmost luxurious dinner party",
+    f"Dear {guests[1].title()} you were selected to attend a dinner party",
+    f"Dear {guests[2].title()}, I'm pleased to announce that you are hearby selected attend the most exclusive dinner party in Fort, Wayne",
+]
+print(the_invitation)
